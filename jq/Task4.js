2d89648aa74656b61b161cb7825102995fadc8d0
gauravgupta01 <50513254+gauravgupta01@users.noreply.github.com>
2023-01-31 10:58:52 +0530

    Add files via upload

---
+++ jq/Task4.js
@@ -0,0 +1,9 @@
+    $(".header").click(function () {
+        $(".header").css("background-color", "red");
+    });
+    $(".sidebar").click(function(){
+        $(".sidebar").text("Leftbar")
+    });
+    $(".extra").click(function(){
+        $(".content").fadeOut("slow")
+    });
